82a308b1880c7664777dd60c2e963db5581e766a
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:03:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
+
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 importScripts(__u22$config.sw || 'uv.sw.js');
 
-const uv = new UVServiceWorker();
+const u2 = new u2ServiceWorker();
 
 async function handleRequest(event) {
-    if (uv.route(event)) {
-        return await uv.fetch(event);
+    if (u2.route(event)) {
+        return await u2.fetch(event);
     }
     
     return await fetch(event.request)
