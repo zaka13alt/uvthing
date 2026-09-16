88d1fbe45e5dff4ce06eec76bfedc6a1a784f3fe
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:08:03 -0500

    Create register-sw.js

---
+++ register-sw.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * Distributed with Ultraviolet and compatible with most configurations.
+ */
+const stocku2t = "/sw.js";
+
+/**
+ * List of hostnames that are allowed to run serviceworkers on http://
+ */
+const u2tAllowedHostnames = ["localhost", "127.0.0.1"];
+
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
+async function registeru2t() {
+	if (!navigator.serviceWorker) {
+		if (
+			location.protocol !== "https:" &&
+			!u2tAllowedHostnames.includes(location.hostname)
+		)
+			throw new Error("Service workers cannot be registered without https.");
+
+		throw new Error("Your browser doesn't support service workers.");
+	}
+
+	await navigator.serviceWorker.register(stocku2t);
+}
