7f5425238d01c495b2209c7aced588e99b111fdc
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:28:35 -0500

    Refactor UV configuration in uv.config.js
    Removed old configuration setup and replaced with a new UV config structure.

--- uv.config.js
+++ uv.config.js
@@ -1,17 +1,13 @@
-(function () {
-    var _href = self.location.href;
-    // If running inside a proxied worker, self.location is the proxied URL
-    var _svc = _href.indexOf('/u22/service/');
-    var _base = _svc !== -1 ? _href.slice(0, _svc) + '/' : new URL('./', _href).href;
-    self.__u22$config = {
-        prefix: new URL('u22/service/', _base).pathname,
-        encodeUrl: u2.codec.xor.encode,
-        decodeUrl: u2.codec.xor.decode,
-        handler: new URL('uv.handler.js', _base).href,
-        client:  new URL('uv.client.js',  _base).href,
-        bundle:  new URL('uv.bundle.js',  _base).href,
-        config:  new URL('uv.config.js',  _base).href,
-        sw:      new URL('uv.sw.js',         _base).href,
-        theBadKeywords: ["pornhub","xvideos","xnxx","xhamster","redtube","youporn","tube8","spankbang","eporner","tnaflix","txxx","pornone","beeg","4tube","drtuber","brazzers","naughtyamerica","realitykings","bangbros","mofos","babes.com","onlyfans","fansly","chaturbate","livejasmin","myfreecams","cam4","stripchat","bongacams","streamate","jerkmate","camsoda","sexier","imlive","porn","xxx","hentai","nsfw","nude","nudes","naked","erotic","fetish","milf","camgirl","sexcam","liveshow","escort","callgirl"],
-    };
-})();
+
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+	prefix: "/uv/service/",
+	encodeUrl: u2.codec.xor.encode,
+	decodeUrl: u2.codec.xor.decode,
+	handler: "uv.handler.js",
+	client: "uv.client.js",
+	bundle: "uv.bundle.js",
+	config: "uv.config.js",
+	sw: "uv.sw.js",
+};
