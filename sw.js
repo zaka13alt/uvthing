9c268cddacc0d34a92e77c8d5bae92a987296da7
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:31:10 -0500

    Fix service worker instantiation and syntax errors

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
+
+/*global u22ServiceWorker,__u22$config*/
+/*
+ 
+ * However, if a user changes the location of u22.bundle.js/u22.config.js or sw.js is not relative to them, they will need to modify this script locally.
+ */
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 importScripts(__u22$config.sw || 'uv.sw.js');
 
-// 
-const u22 = new u2ServiceWorker();
+const u22 = new u22ServiceWorker();
 
 async function handleRequest(event) {
-    // 
     if (u22.route(event)) {
         return await u22.fetch(event);
     }
     
-    return await fetch(event.request);
+    return await fetch(event.request)
 }
 
 self.addEventListener('fetch', (event) => {
