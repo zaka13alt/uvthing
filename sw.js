e98f37e06ae7ae2a95560a783d844cc05681b62c
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:09:47 -0500

    fix the var typo

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ importScripts(__u22$config.sw || 'uv.sw.js');
 const u2 = new u2ServiceWorker();
 
 async function handleRequest(event) {
-    if (u2.route(event)) {
-        return await u2.fetch(event);
+    if (u22.route(event)) {
+        return await u22.fetch(event);
     }
     
     return await fetch(event.request)
