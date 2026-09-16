79260d95ac258f7a02cd30e4419502f3c1f4aac0
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:25:39 -0500

    Refactor u2ServiceWorker initialization and request handling

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
-
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 importScripts(__u22$config.sw || 'uv.sw.js');
 
-const u2 = new u2ServiceWorker();
+// 
+const u22 = new u2ServiceWorker();
 
 async function handleRequest(event) {
+    // 
     if (u22.route(event)) {
         return await u22.fetch(event);
     }
     
-    return await fetch(event.request)
+    return await fetch(event.request);
 }
 
 self.addEventListener('fetch', (event) => {
