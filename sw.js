9f3781f2428700926811adff4ada8a52304d15ea
zaka13alt <zakariyaliban719@gmail.com>
2026-09-16 14:02:32 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  */
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
-importScripts(__uv$config.sw || 'uv.sw.js');
+importScripts(__u22$config.sw || 'uv.sw.js');
 
 const uv = new UVServiceWorker();
 
