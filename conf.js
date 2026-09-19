ef454c169916dd670bc97595e81c252e20a08e64
zaka13alt <zakariyaliban719@gmail.com>
2026-09-18 22:20:59 -0500

    Update service prefix and SW file in conf.js

--- conf.js
+++ conf.js
@@ -1,1 +1,1 @@
-self.__u22$config={prefix:"/u22/service/",encodeUrl:u2.codec.xor.encode,decodeUrl:u2.codec.xor.decode,handler:"/Q3wR8.js",client:"/5j8hx.js",bundle:"/8b3rt.js",config:"/conf.js",sw:"/u22.sw.js"};
+self.__u22$config={prefix:"/5jP9z/",encodeUrl:u2.codec.xor.encode,decodeUrl:u2.codec.xor.decode,handler:"/Q3wR8.js",client:"/5j8hx.js",bundle:"/8b3rt.js",config:"/conf.js",sw:"/1vN6h.js"};
