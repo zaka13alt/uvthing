22503725fc55075096e613eb8220f8c288f0e069
zaka13alt <zakariyaliban719@gmail.com>
2026-09-20 22:39:19 -0500

    Update and rename conf.js to pGJxh.js

---
+++ pGJxh.js
@@ -0,0 +1,1 @@
+self.__u22$config={prefix:"/5jP9z/",encodeUrl:u2.codec.xor.encode,decodeUrl:u2.codec.xor.decode,handler:"/Q3wR8.js",client:"/5j8hx.js",bundle:"/8b3rt.js",config:"/pGJxh.js",sw:"/1vN6h.js"};
